824f82ee99f697c398947dd6d7600d335b04489f
Terra-exe <102436774+Terra-exe@users.noreply.github.com>
2023-07-19 20:10:23 -0700

    Testing G unicorn

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()
+
+
+"""
 #############
 ###IMPORTS###
 #############
@@ -208,3 +220,5 @@ if __name__ == "__main__":
     app.run(debug=True)
 
 application = app
+
+"""
