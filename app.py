ca3894e7841c8b3c4b5788c7557edee45ea5d565
Terra-exe <102436774+Terra-exe@users.noreply.github.com>
2023-07-20 22:23:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -15,9 +15,14 @@ import boto3
 from contextlib import closing
 from io import BytesIO
 
+
+
+
+
 #from flask import Flask
 from flask import Flask, request, jsonify, render_template, send_file, send_from_directory
 import json_builder.bin.kriya_json_builder as kriya_json_builder
+import json_builder.bin.kriya_object as kriya_object
 #import tools.bineural as bineural
 
 
@@ -264,6 +269,83 @@ application = app
 
 @app.route('/convert_text_to_speech', methods=['POST'])
 def convert_text_to_speech():
+    # Initialize clients for Polly and S3
+    polly = boto3.client('polly', region_name='us-west-2')
+    s3 = boto3.client('s3', region_name='us-west-2', aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'), aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'))
+    bucket_name = 'crystal-audio-processing'
+    s3_key = 'audio-dumps/audio-gen-files/Kriya.wav'
+    s3_key_json = 'audio-dumps/audio-gen-files/json_input.json'
+    s3_key_error = 'audio-dumps/audio-gen-files/error.txt'
+    error_file_path = os.path.join("/tmp", s3_key_error)
+
+    try:
+
+        # Getting JSON data from the POST request
+        input_json = request.get_json()
+        
+        # Save the JSON data to a file and upload the file to S3
+        json_file_path = os.path.join("/tmp", s3_key_json)
+        with open(json_file_path, 'w') as json_file:
+            json.dump(input_json, json_file)
+
+        # Upload JSON file to S3
+        s3.upload_file(json_file_path, bucket_name, s3_key_json)
+
+
+    except Exception as e1:
+        with open(error_file_path, 'w') as error_file:
+             error_file.write(str(e1))
+        s3.upload_file(error_file_path, bucket_name, s3_key_error)
+        print(f"Error saving JSON: {e1}")
+        return jsonify({'message': f'Error saving JSON: {e1}'}), 500
+    
+    # Create Kriya object
+    try:
+        kriya_obj = kriya_object.create_kriya_obj_from_json(input_json)
+    except json.JSONDecodeError as e2:
+        with open(error_file_path, 'w') as error_file:
+            error_file.write(str(e2))
+            s3.upload_file(error_file_path, bucket_name, s3_key_error)
+            print(f"Error during Text-To-Speech: {e2}")
+        return jsonify({'message': f'Error decoding JSON: {e2}'}), 400
+
+    # Use Amazon Polly to convert the text to speech
+    try:
+        response = polly.synthesize_speech(Text=kriya_obj.title, OutputFormat='mp3', VoiceId='Salli')
+        
+    except Exception as e3:
+        with open(error_file_path, 'w') as error_file:
+            error_file.write(str(e3))
+        s3.upload_file(error_file_path, bucket_name, s3_key_error)
+        print(f"Error during conversion to speech: {e3}")
+        return jsonify({'message': f'Error during conversion to speech: {e3}'}), 500
+
+    # If we have an audiostream in the response
+    if "AudioStream" in response:
+        with closing(response["AudioStream"]) as stream:
+            output = os.path.join("/tmp", s3_key)
+
+            try:
+                # Open a file for writing the output as a binary stream
+                with open(output, "wb") as file:
+                    file.write(stream.read())
+
+            except IOError as ioe:
+                print(ioe)
+                print(f'Error: File {output} could not be written.')
+                return jsonify({'message': f'Error: File {output} could not be written.'}), 500
+
+            else:
+                # upload the file to s3
+                s3.upload_file(output, bucket_name, s3_key)
+                print(f'File {output} uploaded to {bucket_name} at {s3_key}')
+
+    return jsonify({'message': 'Success'})
+"""previous
+
+
+@app.route('/convert_text_to_speech', methods=['POST'])
+def convert_text_to_speech():
 
     # Initialize clients for Polly and S3
     polly = boto3.client('polly', region_name='us-west-2')
@@ -342,7 +424,7 @@ def create_hello_world_audio():
     
     return jsonify({'message': 'Success'})
 
-
+"""
 
 if __name__ == '__main__':
     app.run()
