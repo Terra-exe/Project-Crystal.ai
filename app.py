5ed9d450bd84d2c23feca33b962d8ebea84006af
Terra-exe <102436774+Terra-exe@users.noreply.github.com>
2023-11-09 21:16:27 -0800

    Update app.py

--- app.py
+++ app.py
@@ -884,7 +884,7 @@ def upload_to_youtube():
         BUCKET_NAME = bucket_name
         TITLE = title
         print('we will now download the audio files following the title: ' + TITLE)
-        download_files_from_s3(BUCKET_NAME, s3_input_file_key, TITLE, download_dir=audio_file_path, default_prefix=TITLE)
+        download_files_from_s3(BUCKET_NAME, s3_input_file_key, TITLE, download_dir=audio_file_path, default_prefix="")
         files = [f for f in os.listdir(audio_file_path) if os.path.isfile(os.path.join(audio_file_path, f))]
         print(files)
 
