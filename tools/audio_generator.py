40a7c790ba0e4ad9df4383a4f35235c020c79544
Terra-exe <102436774+Terra-exe@users.noreply.github.com>
2023-12-13 22:59:47 -0800

    Update audio_generator.py

--- tools/audio_generator.py
+++ tools/audio_generator.py
@@ -188,15 +188,6 @@ class AudioGenerator:
             print(f"Segment {i} length before bytearray conversion: {len(segment)}")
 
         
-            if not isinstance(segment, bytearray):
-            # Check if the segment is in the expected format (e.g., 16-bit stereo)
-                expected_format = 2 * 2  # 2 bytes per sample * 2 channels
-                if len(segment) % expected_format == 0:
-                    print(f"Segment  oops {i} ")
-                    segment = bytearray(segment)
-                else:
-                    # Handle the case where the segment is already in the expected format
-                    pass
 
             # Print length after conversion to bytearray
             print(f"Segment {i} length after bytearray conversion: {len(segment)}")
