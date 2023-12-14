c193acecf83c1401501a80cd7ff42bf2e8421f39
Terra-exe <102436774+Terra-exe@users.noreply.github.com>
2023-12-13 18:49:55 -0800

    Update audio_generator.py

--- tools/audio_generator.py
+++ tools/audio_generator.py
@@ -201,6 +201,13 @@ class AudioGenerator:
 
     def combine_audio_segments(self, input_data, combined_file_path):
             print(f"Arguments received: input_data={input_data}, combined_file_path={combined_file_path}")
+            
+            if not combined_file_path.endswith('.wav'):
+                raise ValueError("The combined_file_path should be a .wav file path, not a directory.")
+
+            # Check if a directory with the same name exists
+            if os.path.isdir(combined_file_path):
+                raise ValueError(f"A directory with the name {combined_file_path} already exists.")
 
             # Initialize variables for combined audio data
             combined_audio_data = bytearray()
