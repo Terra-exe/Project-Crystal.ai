0c3abce2d6f5e7a6654cb1488b555736fc372f57
Terra-exe <102436774+Terra-exe@users.noreply.github.com>
2023-12-13 21:25:01 -0800

    Update audio_generator.py

--- tools/audio_generator.py
+++ tools/audio_generator.py
@@ -191,10 +191,10 @@ class AudioGenerator:
             print(f"Segment {i}: Original frames: {num_frames}")
             
             # Set parameters for the WAV file
-            num_channels = 1  # Mono=1, Stereo=2
+            num_channels = 2  # Mono=1, Stereo=2
             sample_width = 2  # 2 bytes (16 bits) per sample
             sample_rate = 44100  # Sampling frequency in Hz
-            num_frames = len(segment) // (num_channels * sample_width)
+            num_frames = (len(segment)/2) // (num_channels * sample_width)
 
             # Save the segment as a WAV file
             with wave.open(file_path, 'wb') as wav_file:
