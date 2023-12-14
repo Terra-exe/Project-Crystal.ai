b3ee74d39dfdecb677fe7a0b880fae34e3d4a873
Terra-exe <102436774+Terra-exe@users.noreply.github.com>
2023-12-13 22:16:22 -0800

    Update audio_generator.py

--- tools/audio_generator.py
+++ tools/audio_generator.py
@@ -188,7 +188,7 @@ class AudioGenerator:
             wav_file_paths.append(file_path)
         
             # Calculate the number of frames for this segment
-            num_frames = len(segment)  # Assuming 16-bit stereo audio
+            num_frames = len(segment) // (2 * 2)  # Assuming 16-bit stereo audio
             print(f"Segment {i}: Original frames: {num_frames}")
             
             # Set parameters for the WAV file
